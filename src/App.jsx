78b67ddc23030c6e6c8722f4d4f55157e6da5134
Charles Callender <charlescallender.cc@gmail.com>
2022-09-20 09:57:01 -0700

    Initial commit. Built framework

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.scss';
+import Projects from './Components/Projects/Projects';
+
+function App() {
+  return (
+    <div className="App">
+      
+        
+        <h1>MySQL Projects</h1>
+        <Projects />
+      
+    </div>
+  );
+}
+
+export default App;
