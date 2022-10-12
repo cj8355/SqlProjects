3538e8337bfa7983edc397ce774f8e6b4bebb994
Charles Callender <charlescallender.cc@gmail.com>
2022-10-11 20:36:09 -0700

    Added project carousel

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,10 +1,72 @@
-
+import "./projects.scss";
+import { useState } from "react";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function Projects() {
+    const [currentSlide,setCurrentSlide] = useState(0);
+
+    const data = [
+        {
+            id: "1",
+            icon: "assets/globe.png",
+            title: "Proj 1",
+            desc: "1st MySQL Project",
+            img: "assets/retroland.png",
+            livesite: "http://cj8355.github.io/RetroLand",
+            repo: "https://github.com/cj8355/RetroLand",
+            techUsed: [ "React", "Styled Components", "Firebase", "Material UI"]
+        },
+       
+    ];
+    
+    const handleClick = (way)=> {
+        way === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2) :
+        setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+    }
+
     return (
         <div className="Container">
             Projects
-        
+            <div className="slider" style={{transform: `translateX(-${currentSlide * 100}vw)` }}>
+                {data.map((d) => (
+                <div className="container" key={d.id}>
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src={process.env.PUBLIC_URL + "/" +  d.icon} alt="" />
+                                </div>
+                                <h2>{d.title}</h2>
+                                <p>{d.desc}</p>
+                                <div className="iconContainer">
+                                <a href={d.livesite} target="_blank"> <LanguageOutlinedIcon className="websiteIcon" /> </a>
+                                <a href={d.repo} target="_blank"> <GitHubIcon className="gitHubIcon" /> </a>
+                                </div>
+                                
+                                {/* {d.techUsed.length && (
+                                    <ul className="techUsed">
+                                        {d.techUsed.map((tech, i) => (
+                                    <li key={i}>{tech}</li>
+                                    ))}
+                                    </ul>
+                                    )} */}
+                                    
+                            </div>
+                        </div>
+                        <div className="right">
+                            <img src={process.env.PUBLIC_URL + "/" +  d.img} alt="" />
+                        </div>
+                    </div>
+                    
+                </div>
+                ))}
+            </div>
+            <ArrowBackIosIcon className="arrow left" onClick={()=>handleClick("left")}/>
+            <ArrowForwardIosIcon className="arrow right"  onClick={()=>handleClick("right")}/>
         </div>
+        
     )
 }
